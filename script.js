fe263b94b3efd39c7cbdde2621da0a2fb73e2b00
Alvin Faiz <softkopi3258@gmail.com>
2021-02-01 16:14:44 +0700

    Perbarui tanggal

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const tglNow = new Date().getDate();
-tglRam = tglNow + 7;
+const date1 = new Date().getDate();
+const date2 = new Date('12/4/2021');
+const date3 = new Date('12/5/2021');
+const diffTime = Math.abs(date2 - date1);
+const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+const diffTime1 = Math.abs(date2 - date1);
+const diffDays1 = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+
+
+tglRam = diffDays;
 toSyawal = 24-tglNow;
 
-$('#quote').text(`${tglRam}  Ramadhan. Lebaran ${toSyawal} hari lagi.`);
+$('#quote').text(`Ramadhan ${diffDays}  Hari lagi. Lebaran ${diffDays1} hari lagi.`);
